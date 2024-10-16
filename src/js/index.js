554f7d3856317b1cdcd54637d0b797b776010081
Genezis <ucmessina@alumnostrazos.net>
2024-10-16 14:58:49 +0200

    +Update

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,29 @@ import '../scss/styles.scss';
 import { toggleMenuElement } from './dom';
 import { toggleMenu } from './menu';
 
+const words = ['Quality', 'Professionalism', 'Compromise'];
+const span = document.getElementById('word');
+let wordIndex = 0;
+let letterIndex = 0;
+let deleting = false;
+
+function type() {
+  const currentWord = `#${words[wordIndex]}`;
+  span.textContent = deleting ? currentWord.slice(0, --letterIndex) : currentWord.slice(0, ++letterIndex);
+
+  if (!deleting && letterIndex === currentWord.length) {
+    deleting = true;
+    setTimeout(type, 700);
+  } else if (deleting && letterIndex === 1) {
+    deleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(type, 300);
+  } else {
+    const speed = deleting ? 100 : 150;
+    setTimeout(type, speed);
+  }
+}
+
+type();
+
 toggleMenuElement.addEventListener('click', toggleMenu);

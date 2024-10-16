f840f1de3ded4211fe69681e772d5fe3dcebc3ac
Genezis <ucmessina@alumnostrazos.net>
2024-10-16 13:54:27 +0200

    +Update top-bar

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,4 +1,5 @@
 const toggleMenuElement = document.getElementById('toggle-menu');
 const menuElement = document.getElementById('menu');
+const topBarElement = document.getElementById('top-bar');
 
-export { menuElement, toggleMenuElement };
+export { menuElement, toggleMenuElement, topBarElement };
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,8 @@
-import { menuElement } from './dom';
+import { menuElement, topBarElement } from './dom';
 
 const toggleMenu = () => {
   menuElement.classList.toggle('menu--show');
+  topBarElement.classList.toggle('top-bar--open');
 };
 
 export { toggleMenu };

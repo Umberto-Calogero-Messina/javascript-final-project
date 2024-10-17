1adc69aa3c2e4ac8043c41eb8ec45b1555de9b3c
Genezis <ucmessina@alumnostrazos.net>
2024-10-17 11:11:14 +0200

    +GamingSection

--- src/js/index.js
+++ src/js/index.js
@@ -4,29 +4,29 @@ import '../scss/styles.scss';
 import { toggleMenuElement } from './dom';
 import { toggleMenu } from './menu';
 
-const words = ['Game On!', 'Quality!'];
-const span = document.getElementById('word');
-let wordIndex = 0;
-let letterIndex = 0;
-let deleting = false;
+// const words = ['Game On!', 'Quality!'];
+// const span = document.getElementById('word');
+// let wordIndex = 0;
+// let letterIndex = 0;
+// let deleting = false;
 
-function type() {
-  const currentWord = `¡${words[wordIndex]}`;
-  span.textContent = deleting ? currentWord.slice(0, --letterIndex) : currentWord.slice(0, ++letterIndex);
+// function type() {
+//   const currentWord = `¡${words[wordIndex]}`;
+//   span.textContent = deleting ? currentWord.slice(0, --letterIndex) : currentWord.slice(0, ++letterIndex);
 
-  if (!deleting && letterIndex === currentWord.length) {
-    deleting = true;
-    setTimeout(type, 700);
-  } else if (deleting && letterIndex === 1) {
-    deleting = false;
-    wordIndex = (wordIndex + 1) % words.length;
-    setTimeout(type, 300);
-  } else {
-    const speed = deleting ? 100 : 150;
-    setTimeout(type, speed);
-  }
-}
+//   if (!deleting && letterIndex === currentWord.length) {
+//     deleting = true;
+//     setTimeout(type, 700);
+//   } else if (deleting && letterIndex === 1) {
+//     deleting = false;
+//     wordIndex = (wordIndex + 1) % words.length;
+//     setTimeout(type, 300);
+//   } else {
+//     const speed = deleting ? 100 : 150;
+//     setTimeout(type, speed);
+//   }
+// }
 
-type();
+// type();
 
 toggleMenuElement.addEventListener('click', toggleMenu);

4ceba7d1d35cb6cc76896e68d8cce6a101643588
josefainostroza <62558856+josefainostroza@users.noreply.github.com>
2024-10-22 12:04:56 +0200

    +Update

--- src/js/newsData.js
+++ src/js/newsData.js
@@ -25,8 +25,8 @@ export const newsData = [
   },
   {
     date: '11.11.17',
-    status: 'Removed',
-    title: 'The best online game is out now!',
+    status: 'Added',
+    title: 'Start project',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida....',
   },
 ];
